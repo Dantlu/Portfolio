8e635e06e9e2257c277240b36f07a0a383ba467e
Dante_dev <109294689+Dantlu@users.noreply.github.com>
2025-01-20 12:29:16 -0300

    Js del proyecto

--- JS/main.js
+++ JS/main.js
@@ -4,7 +4,7 @@ let menuIcon = document.querySelector('#menu-icon');
 let navBar = document.querySelector('.nabvar');
 
 menuIcon.onclick= () => {
-    menuIcon.classList.toggle(fa-xmark);
+    menuIcon.classList.toggle('fa-xmark');
     navBar.classList.toggle('active');
 };
 
@@ -15,10 +15,10 @@ window.onscroll = () =>{
     sections.forEach(sec =>{
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
-        let heigh = sec.offsetHeigh;
+        let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + heigh){
+        if(top >= offset && top < offset + height){
             navLinks.forEach.apply(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id +']').classList.add('active');
@@ -32,3 +32,22 @@ window.onscroll = () =>{
     menuIcon.classList.remove('fa-xmark');
     navBar.classList.remove('active');
 };
+
+ScrollReveal({ 
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, heading', { origin:'top'});
+ScrollReveal().reveal('.home-img, .stack-container, .portfolio-box, .contact form', { origin:'buttom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin:'left'});
+ScrollReveal().reveal('.home-content p, .about-content', { origin:'right'});
+
+const typed = new Typed('.multiple-text',{
+    strings: ['Analista de Datos', 'Desarrollador'],
+    typeSpeed:70,
+    backSpeed:70,
+    backDelay:1000,
+    loop:true,
+});
